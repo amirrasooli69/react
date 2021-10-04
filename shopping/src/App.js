7ca1ef73c8b0aa576c82ca57bf1ca699276283ca
Amir <amirrasooli69@gmail.com>
2021-10-05 00:34:55 +0330

    crate page home page shoping

--- shopping/src/App.js
+++ shopping/src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar } from './component/navbar';
+import { HomePage } from './pages';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <HomePage/>
     </div>
   );
 }
